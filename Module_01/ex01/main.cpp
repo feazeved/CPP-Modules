747a03841a32c724203acccc6afa10094a2f4629
Felipe Azevedo Soares Suassuna <feazeved@Mac.Home>
2026-05-20 12:20:41 +0100

    finished Module01-ex02

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     Zombie *ptr = zombieHorde(3, "Felipe");
-	ptr[0].announce();
-	ptr[1].announce();
-	ptr[2].announce();
+    ptr[0].announce();
+    ptr[1].announce();
+    ptr[2].announce();
     delete[] ptr;
     return (0);
 }
