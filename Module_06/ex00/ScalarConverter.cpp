8c02309fd90a985b910bc74a3050a5e82b62bb7c
Felipe Azevedo Soares Suassuna <feazeved@Felipes-Air.lan>
2026-06-21 00:10:37 +0100

    implemented basic checks M06-ex00

--- Module_06/ex00/ScalarConverter.cpp
+++ Module_06/ex00/ScalarConverter.cpp
@@ -21,31 +21,77 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 ScalarConverter::~ScalarConverter() { }
 
-// PUBLIC METHODS
-void	ScalarConverter::convert(const std::string& str)
+static void	stt_trim(std::string& str)
 {
-	char	*endptr;
-	double	value;
-	size_t	pos;
+	const char*	begin;
+	size_t		start_pos;
+	size_t		toDelete;
 
-	if (str.empty())
+	if (!str.at(0))
+		return ;
+
+	begin = str.c_str();
+	start_pos = str.find_first_not_of(" ");
+
+	toDelete = static_cast<size_t>((begin + start_pos) - begin);
+	str.erase(0, toDelete);
+
+
+	const char* end;
+	size_t		end_pos;
+
+	begin = str.c_str();
+	end = str.c_str() + str.length() - 1;
+	end_pos = str.find_last_not_of(" ");
+
+	toDelete = static_cast<size_t>(end - (begin + end_pos));
+	str.erase(end_pos, toDelete);
+}
+
+static bool stt_isHex(const std::string& str)
+{
+	if (str.at(0) == '0' && (str.at(1) == 'x' || str.at(1) == 'X'))
+		return (true);
+	return (false);
+}
+
+static bool	stt_isOct(const std::string& str)
+{
+	if (str.at(0) == '0' && (str.at(1) >= '0' && str.at(1) <= '9'))
+		return (true);
+	return (false);
+}
+
+static bool	stt_strtodInvalid(const char* startptr, char *endptr)
+{
+	if (errno == ERANGE
+		|| startptr == endptr
+		|| (*endptr && *endptr != 'f')
+		|| (*endptr == 'f' && *(endptr + 1)))
+	{
+		std::cerr << "Error: Invalid input.\n";
+		return (true);
+	}
+	return (false);
+}
+
+// PUBLIC METHODS
+void	ScalarConverter::convert(std::string str)
+{
+	const char	*startptr;
+	char	*endptr = NULL;
+	double	value;
+
+	stt_trim(str);
+	if (str.empty() || stt_isHex(str) || stt_isOct(str))
 		return ;
 
 	errno = 0;
-	value = std::strtod(str.c_str(), &endptr);
-	pos = str.find_first_not_of(ScalarConverter::digits);
-	
+	startptr = str.c_str();
+	value = std::strtod(startptr, &endptr);
 
-
-	//pos = str.find_first_not_of("DIGITS");
-
-	std::cout << ScalarConverter::digits << "\n";
-	if (*endptr)
-	{
-		double temp = std::strtod(str.c_str(), &endptr);
-		
+	if (stt_strtodInvalid(startptr, endptr))
 		return ;
-	}
 
 	std::cout << value << "\n";
 }
--- Module_06/ex00/ScalarConverter.hpp
+++ Module_06/ex00/ScalarConverter.hpp
@@ -12,7 +12,7 @@ public:
 
 	};
 
-	static void	convert(const std::string& str);
+	static void	convert(std::string str);
 
 private:
 	ScalarConverter();
