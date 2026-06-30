1a8557b3838d6496dfa4e006195a8c9b409f9b81
Felipe Azevedo Soares Suassuna <feazeved@Felipes-MacBook-Air.local>
2026-06-30 19:52:03 +0100

    started M09-ex00.

--- Module_09/ex00/BitcoinExchange.hpp
+++ Module_09/ex00/BitcoinExchange.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <fstream>
+#include <string>
+#include <map>
+
+class BitcoinExchange {
+public:
+	BitcoinExchange(std::ifstream& file);
+	BitcoinExchange(const BitcoinExchange& other);
+	BitcoinExchange& operator=(const BitcoinExchange& other);
+	~BitcoinExchange();
+
+private:
+	std::map<std::string, float>	m;
+};
--- Module_09/ex00/main.cpp
+++ Module_09/ex00/main.cpp
@@ -1,9 +1,46 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
 #include "BitcoinExchange.hpp"
+
+using std::cerr;
+using std::endl;
 
 int	main(int argc, char **argv)
 {
+	if (argc != 2) {
+		cerr << "Error: Usage -> ./btc <input_file>" << endl;
+		return (1);
+	}
 
+	const std::string	databaseName("data.csv");
+	const std::string	requestFileName(argv[1]);
 
+	std::ifstream		database;
+	std::ifstream		requestFile;
 
+	errno = 0;
+
+	database.open(databaseName.c_str());
+	if (!database.is_open()) {
+		cerr << "Error: Could not open data.csv: " << std::strerror(errno) << endl;
+		return (1);
+	}
+
+	requestFile.open(requestFileName.c_str());
+	if (!requestFile.is_open()) {
+		cerr << "Error: Could not open " + requestFileName << ": " << std::strerror(errno) << endl;
+		return (1);
+	}
+
+	std::string	line;
+
+	std::getline(requestFile, line);
+
+	std::cout << line << "\n";
+
+	// BitcoinExchange	mainDatabase(database);
+	// BitcoinExchange	requestDatabase(requestFile);
 
 }
